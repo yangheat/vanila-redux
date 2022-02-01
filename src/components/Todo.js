cdbd0b0058fcb2263586bd88b0e4c3e9dc19b6a9
sunpark <yangheat@gmail.com>
2022-02-01 23:43:23 +0900

    Deleteing to do

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+
+function Todo({ text, onBtnClick }) {
+  return (
+    <li>
+      {text} <button onClick={onBtnClick}>DEL</button>
+    </li>
+  );
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  console.log(dispatch, ownProps);
+  return { onBtnClick: () => dispatch(actionCreators.deleteTodo(ownProps.id)) };
+}
+
+export default connect(null, mapDispatchToProps)(Todo);
