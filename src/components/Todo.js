c6d03d8ba51d824024d6981c41ea40b2a6113f60
sunpark <yangheat@gmail.com>
2022-02-04 17:50:19 +0900

    code cleanup

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreators } from "../store";
 import Delete from "./Delete";
 
 function Todo({ text, id, onBtnClick }) {
   return (
     <li>
       <Link to={`${id}`}>{text}</Link>
-      {/* <button onClick={onBtnClick}>DEL</button> */}
       <Delete id={id} />
     </li>
   );
 }
 
-function mapDispatchToProps(dispatch, ownProps) {
-  // 해당 위치에 함수를 추가하게 되면 props에 추가됨.
-  return { onBtnClick: () => dispatch(actionCreators.deleteTodo(ownProps.id)) };
-}
-
-export default connect(null, mapDispatchToProps)(Todo);
+export default Todo;
